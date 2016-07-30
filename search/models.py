c3f63e34306082294370f5708e456f410006de7a
spawnreed <christopherreed@Christophers-MacBook-Pro.local>
2016-07-29 23:05:32 -0400

    file upload

--- search/models.py
+++ search/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Board(models.Model):
     board_name = models.CharField(max_length=250)
     board_votes = models.IntegerField(default=0)
+    board_icon = models.FileField(default="none")
 
     def __str__(self):
         return self.board_name
@@ -15,6 +16,7 @@ class Response(models.Model):
     response_text = models.CharField(max_length=1000, default='default')
     response_votes = models.IntegerField(default=0)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    image = models.FileField(default="none")
 
     def __str__(self):
         return self.response_text
